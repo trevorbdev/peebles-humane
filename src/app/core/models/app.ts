705594d2523a005423aec918580d6a0c3b6efd68
Trevor <trevor@innov.dev>
2021-04-18 20:06:10 -0500

    app comma fix

--- src/app/core/models/app.ts
+++ src/app/core/models/app.ts
@@ -1,27 +1,27 @@
 export interface App {
-    id: string,
-    petid: string | undefined,
-    event_uuid: string,
-    firstname: string,
-    lastname: string,
-    email: string,
-    dln: string,
-    birthday: string,
-    spousefirst: string,
-    spouselast: string,
-    phone: string,
-    address: string,
-    city: string,
-    state: string,
-    zip: string,
-    home: string,
-    landlord: string,
-    hometype: string,
-    moving: string,
-    residents: string,
-    children: string,
-    animals: string,
-    status: string,
-    appdate: string,
-    reason: string | null,
+    id: string;
+    petid: string | undefined;
+    event_uuid: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    dln: string;
+    birthday: string;
+    spousefirst: string;
+    spouselast: string;
+    phone: string;
+    address: string;
+    city: string;
+    state: string;
+    zip: string;
+    home: string;
+    landlord: string;
+    hometype: string;
+    moving: string;
+    residents: string;
+    children: string;
+    animals: string;
+    status: string;
+    appdate: string;
+    reason: string | null;
 }
